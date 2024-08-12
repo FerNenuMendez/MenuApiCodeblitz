5bc7608a85a2c700469593d5b4e6dfa1ec04b82d
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-08-12 14:13:57 -0300

    pruebas recuperar contraseña - fixing bugs3

--- src/controllers/resetP.controller.js
+++ src/controllers/resetP.controller.js
@@ -11,7 +11,16 @@ export const forgotPassword = async (req, res) => {
             logger.error('Cliente a restablecer contraseña no encontrado')
             return res.status(404).send('Cliente not found');
         }
-
+        const token = crypto.randomBytes(20).toString('hex');
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hora
+        const resetURL = `http:${req.headers.host}/reset/${token}`;
+        const message = `
+         <h1>Password Reset Request</h1>
+         <p>You requested a password reset</p>
+         <p>Click this <a href="${resetURL}">link</a> to reset your password</p>
+         `;
+        await sendMail(user.mail, 'Password Reset', message);
         logger.info(`Usuario encontrado: ${JSON.stringify(user, null, 2)}`)
         res.status(200).send('Password reset email sent');
     } catch (err) {
